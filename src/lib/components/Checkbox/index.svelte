2b43983404d21d08a82a1accd49418730f7610ff
Kai Magnus Müller <ndsignin@outlook.de>
2024-11-15 21:33:16 +0100

    Add Checkbox component
    - Add Checkbox component
    - Fix Section wrapping

---
+++ src/lib/components/Checkbox/index.svelte
@@ -0,0 +1,127 @@
+<script lang="ts">
+	import { blurOnEvent } from '$lib/helpers.svelte.js';
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	type Props = {
+		onchange?: (e: Event) => void;
+		onfocus?: (e: Event) => void;
+		onblur?: (e: Event) => void;
+		id?: string;
+		checked?: boolean;
+		disabled?: boolean;
+		tabindex?: number;
+		class?: string;
+		value?: string;
+		children?: import('svelte').Snippet;
+		[key: string]: unknown;
+	} & HTMLInputAttributes;
+
+	let {
+		onchange,
+		onfocus,
+		onblur,
+		id = 'fk-' + Math.random().toString(36),
+		checked = $bindable(false),
+		disabled = false,
+		tabindex = 0,
+		class: className = '',
+		value,
+		children,
+		...props
+	}: Props = $props();
+</script>
+
+<label for={id}>
+	<input
+		{...props}
+		type="checkbox"
+		{id}
+		bind:checked
+		{disabled}
+		{tabindex}
+		{value}
+		class={className}
+		{onchange}
+		{onfocus}
+		{onblur}
+		use:blurOnEvent
+	/>
+	{@render children?.()}
+
+	<!-- 
+	<svg class="svg" xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8"><path fill="#fff" fill-opacity="1" fill-rule="evenodd" stroke="none" d="M1.176 2.824 3.06 4.706 6.824.941 8 2.118 3.059 7.059 0 4z"></path></svg>
+	-->
+</label>
+
+<style>
+	label {
+		display: flex;
+		align-items: center;
+		font-size: var(--font-size-small);
+		user-select: none;
+	}
+
+	input {
+		position: relative;
+		appearance: none;
+		margin: 0;
+		padding: 0;
+		width: 2rem;
+		height: 2rem;
+	}
+
+	input::before {
+		display: block;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		appearance: none;
+		border: 1px solid var(--figma-color-icon);
+		border-radius: var(--border-radius-small);
+		width: 0.75rem;
+		height: 0.75rem;
+		content: '';
+	}
+
+	input:checked::before {
+		border-color: transparent;
+		background-image: url("data:image/svg+xml,%3Csvg class='svg' xmlns='http://www.w3.org/2000/svg' width='8' height='8' viewBox='0 0 8 8'%3E%3Cpath fill='%23fff' fill-opacity='1' fill-rule='evenodd' stroke='none' d='M1.176 2.824 3.06 4.706 6.824.941 8 2.118 3.059 7.059 0 4z'%3E%3C/path%3E%3C/svg%3E");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-color: var(--figma-color-bg-brand);
+	}
+
+	label:has(input:disabled) {
+		color: var(--figma-color-text-disabled);
+	}
+
+	input:disabled::before {
+		border: 1px solid var(--figma-color-icon-disabled);
+	}
+
+	input:checked:disabled::before {
+		border: 1px solid transparent;
+		background-color: var(--figma-color-icon-disabled);
+	}
+
+	/* label {
+		display: flex;
+		padding: var(--size-xxsmall) var(--size-xsmall) var(--size-xxsmall) var(--size-small);
+		color: var(--figma-color-text);
+		font-weight: var(--font-weight-normal);
+		font-size: var(--font-size-xsmall);
+		user-select: none;
+	} */
+
+	/* input:enabled:checked:focus + label:before {
+		box-shadow: 0 0 0 2px var(--figma-color-border-brand-strong);
+		border: 1px solid var(--figma-color-bg);
+		border-radius: var(--border-radius-small);
+	}
+
+	input:enabled:focus + label:before {
+		box-shadow: 0 0 0 1px var(--figma-color-border-brand-strong);
+		border: 1px solid var(--figma-color-border-brand-strong);
+	} */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,16 @@
 	import '$lib/css/resets.css';
 
 	// Imports for demo components
-	import { Button, IconButton, IconNodeBooleanOperation, IconInstance } from '$lib/index.js';
+	import {
+		Button,
+		IconButton,
+		IconNodeBooleanOperation,
+		IconInstance,
+		Checkbox
+	} from '$lib/index.js';
+
+	let buttonCounter = $state(0);
+	let checked = $state(true);
 </script>
 
 <svelte:head>
@@ -21,25 +30,29 @@
 
 <LayoutContainer>
 	<Section title="Buttons">
-		<Button onclick={() => console.log('Button 1 clicked')}>Button 1</Button>
+		<Button
+			onclick={(e) => {
+				console.log('Button 1 clicked'), buttonCounter++;
+			}}>Button 1 clicked {buttonCounter} times</Button
+		>
 		<Button variant="secondary" onclick={() => console.log('Button 2 clicked')}>Button 2</Button
 		>
 		<Button variant="tertiary" onclick={() => console.log('Button 3 clicked')}>Button 3</Button>
 		<Button destructive onclick={() => console.log('Button 4 clicked')}>Button 4</Button>
 		<Button disabled onclick={() => console.log('Button 5 clicked')}>Button 5</Button>
-		<Button disabled destructive onclick={() => console.log('Button 5 clicked')}
-			>Button 5</Button
+		<Button disabled destructive onclick={() => console.log('Button 6 clicked')}
+			>Button 6</Button
 		>
-		<Button icon={IconNodeBooleanOperation} onclick={() => console.log('Button 1 clicked')}
-			>Button 1</Button
+		<Button icon={IconNodeBooleanOperation} onclick={() => console.log('Button 7 clicked')}
+			>Button 7</Button
 		>
 		<Button
 			icon={IconNodeBooleanOperation}
 			variant="secondary"
-			onclick={() => console.log('Button 2 clicked')}>Button 2</Button
+			onclick={() => console.log('Button 8 clicked')}>Button 8</Button
 		>
 
-		<Button rounded onclick={() => console.log('Button 6 clicked')}>Button 6</Button>
+		<Button rounded onclick={() => console.log('Button 9 clicked')}>Button 9</Button>
 	</Section>
 
 	<Section title="Icon Buttons">
@@ -48,6 +61,24 @@
 			onclick={() => console.log('Icon Button 1 clicked')}
 		/>
 		<IconButton icon={IconInstance} onclick={() => console.log('Icon Button 2 clicked')} />
+	</Section>
+
+	<Section title="Checkbox">
+		<Checkbox
+			onchange={(e) => console.log('Checkbox 1 clicked', e.currentTarget as HTMLInputElement)}
+			>Checkbox 1</Checkbox
+		>
+		<Checkbox onchange={() => console.log('Checkbox 2 clicked')} disabled>Checkbox 2</Checkbox>
+		<Checkbox
+			onchange={(e) => {
+				checked = (e.currentTarget as HTMLInputElement).checked;
+				console.log('Checkbox 3 clicked', (e.currentTarget as HTMLInputElement).checked);
+			}}
+			{checked}>Checkbox 3 is {checked}</Checkbox
+		>
+		<Checkbox onchange={() => console.log('Checkbox 4 clicked')} checked disabled
+			>Checkbox 4</Checkbox
+		>
 	</Section>
 </LayoutContainer>
 
