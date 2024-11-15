e987b6306a877de10a87cc4c866bc221a40b5998
Kai Magnus Müller <ndsignin@outlook.de>
2024-11-15 16:05:51 +0100

    Add Layout component for overview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	// Imports for showcase
 	import Section from './components/Section.svelte';
+	import LayoutContainer from './components/LayoutContainer.svelte';
 
 	// Global styles
 	import '$lib/css/global.css';
@@ -18,32 +19,37 @@
 
 <p>A component library for Svelte based Figma plugins.</p>
 
-<Section title="Buttons">
-	<Button onclick={() => console.log('Button 1 clicked')}>Button 1</Button>
-	<Button variant="secondary" onclick={() => console.log('Button 2 clicked')}>Button 2</Button>
-	<Button variant="tertiary" onclick={() => console.log('Button 3 clicked')}>Button 3</Button>
-	<Button destructive onclick={() => console.log('Button 4 clicked')}>Button 4</Button>
-	<Button disabled onclick={() => console.log('Button 5 clicked')}>Button 5</Button>
-	<Button disabled destructive onclick={() => console.log('Button 5 clicked')}>Button 5</Button>
-	<Button icon={IconNodeBooleanOperation} onclick={() => console.log('Button 1 clicked')}
-		>Button 1</Button
-	>
-	<Button
-		icon={IconNodeBooleanOperation}
-		variant="secondary"
-		onclick={() => console.log('Button 2 clicked')}>Button 2</Button
-	>
+<LayoutContainer>
+	<Section title="Buttons">
+		<Button onclick={() => console.log('Button 1 clicked')}>Button 1</Button>
+		<Button variant="secondary" onclick={() => console.log('Button 2 clicked')}>Button 2</Button
+		>
+		<Button variant="tertiary" onclick={() => console.log('Button 3 clicked')}>Button 3</Button>
+		<Button destructive onclick={() => console.log('Button 4 clicked')}>Button 4</Button>
+		<Button disabled onclick={() => console.log('Button 5 clicked')}>Button 5</Button>
+		<Button disabled destructive onclick={() => console.log('Button 5 clicked')}
+			>Button 5</Button
+		>
+		<Button icon={IconNodeBooleanOperation} onclick={() => console.log('Button 1 clicked')}
+			>Button 1</Button
+		>
+		<Button
+			icon={IconNodeBooleanOperation}
+			variant="secondary"
+			onclick={() => console.log('Button 2 clicked')}>Button 2</Button
+		>
 
-	<Button rounded onclick={() => console.log('Button 6 clicked')}>Button 6</Button>
-</Section>
+		<Button rounded onclick={() => console.log('Button 6 clicked')}>Button 6</Button>
+	</Section>
 
-<Section title="Icon Buttons">
-	<IconButton
-		icon={IconNodeBooleanOperation}
-		onclick={() => console.log('Icon Button 1 clicked')}
-	/>
-	<IconButton icon={IconInstance} onclick={() => console.log('Icon Button 2 clicked')} />
-</Section>
+	<Section title="Icon Buttons">
+		<IconButton
+			icon={IconNodeBooleanOperation}
+			onclick={() => console.log('Icon Button 1 clicked')}
+		/>
+		<IconButton icon={IconInstance} onclick={() => console.log('Icon Button 2 clicked')} />
+	</Section>
+</LayoutContainer>
 
 <style>
 	h1 {
