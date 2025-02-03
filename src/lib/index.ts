b211eebedf3fd9a816f5dcb435ec182dbce05336
Kai Magnus Müller <ndsignin@outlook.de>
2025-02-03 15:23:53 +0100

    Update ui3 icons (#34)
    * Add and update icons
    
    * Updated demo page and new icons

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -15,6 +15,7 @@ import Textarea from './components/Textarea/index.svelte';
 import Type from './components/Type/index.svelte';
 
 //Icons
+
 import IconAdjust from './icons/adjust.svg?raw';
 import IconAlert from './icons/alert.svg?raw';
 import IconAlignHorizontalCenters from './icons/layout-align-horizontal-centers.svg?raw';
@@ -24,7 +25,10 @@ import IconAlignTop from './icons/layout-align-top.svg?raw';
 import IconAlignVerticalCenters from './icons/layout-align-vertical-centers.svg?raw';
 import IconAngle from './icons/angle.svg?raw';
 import IconArrowLeftRight from './icons/arrow-left-right.svg?raw';
+import IconAutoLayoutAdd from './icons/auto-layout-add.svg?raw';
 import IconAutoLayoutHorizontal from './icons/auto-layout-horizontal.svg?raw';
+import IconAutoLayoutIgnore from './icons/auto-layout-ignore.svg?raw';
+import IconAutoLayoutRemove from './icons/auto-layout-remove.svg?raw';
 import IconAutoLayoutVertical from './icons/auto-layout-vertical.svg?raw';
 import IconBack from './icons/back.svg?raw';
 import IconBlend from './icons/blend.svg?raw';
@@ -35,8 +39,11 @@ import IconCaretLeft from './icons/caret-left.svg?raw';
 import IconCaretRight from './icons/caret-right.svg?raw';
 import IconCaretUp from './icons/caret-up.svg?raw';
 import IconCheck from './icons/check.svg?raw';
+import IconChevronDown from './icons/chevron-down.svg?raw';
+import IconChevronRight from './icons/chevron-right.svg?raw';
 import IconClose from './icons/close.svg?raw';
-import IconComponent from './icons/component.svg?raw';
+import IconCode from './icons/code.svg?raw';
+import IconComponentMultiple from './icons/component-multiple.svg?raw';
 import IconCornerRadius from './icons/corner-radius.svg?raw';
 import IconCorners from './icons/corners.svg?raw';
 import IconDistributeHorizontalSpacing from './icons/distribute-horizontal-spacing.svg?raw';
@@ -45,14 +52,17 @@ import IconDraft from './icons/draft.svg?raw';
 import IconEffects from './icons/effects.svg?raw';
 import IconEllipses from './icons/ellipses.svg?raw';
 import IconEyedropper from './icons/eyedropper.svg?raw';
+import IconFlatten from './icons/flatten.svg?raw';
+import IconFlipHorizontal from './icons/flip-horizontal.svg?raw';
+import IconFlipVertical from './icons/flip-vertical.svg?raw';
+import IconFolder from './icons/folder.svg?raw';
 import IconForward from './icons/forward.svg?raw';
-import IconFrame from './icons/frame.svg?raw';
-import IconGroup from './icons/group.svg?raw';
+import IconGapHorizontal from './icons/gap-horizontal.svg?raw';
+import IconGapVertical from './icons/gap-vertical.svg?raw';
+import IconGradient from './icons/gradient.svg?raw';
 import IconHidden from './icons/hidden.svg?raw';
-import IconHorizontalPadding from './icons/horizontal-padding.svg?raw';
 import IconHyperlink from './icons/hyperlink.svg?raw';
 import IconImage from './icons/image.svg?raw';
-import IconInstance from './icons/instance.svg?raw';
 import IconKey from './icons/key.svg?raw';
 import IconLayoutAlignBottom from './icons/layout-align-bottom.svg?raw';
 import IconLayoutGridColumns from './icons/layout-grid-columns.svg?raw';
@@ -66,7 +76,13 @@ import IconListDetailed from './icons/list-detailed.svg?raw';
 import IconListTile from './icons/list-tile.svg?raw';
 import IconLockOff from './icons/lock-off.svg?raw';
 import IconLockOn from './icons/lock-on.svg?raw';
+import IconMaskAdd from './icons/mask-add.svg?raw';
+import IconMaskExclude from './icons/mask-exclude.svg?raw';
+import IconMaskIntersect from './icons/mask-intersect.svg?raw';
+import IconMaskSubtract from './icons/mask-subtract.svg?raw';
+import IconMaskUnion from './icons/mask-union.svg?raw';
 import IconMinus from './icons/minus.svg?raw';
+import IconPathEdit from './icons/path-edit.svg?raw';
 import IconPlay from './icons/play.svg?raw';
 import IconPlus from './icons/plus.svg?raw';
 import IconRandom from './icons/random.svg?raw';
@@ -75,15 +91,18 @@ import IconResizeToFit from './icons/resize-to-fit.svg?raw';
 import IconResolve from './icons/resolve.svg?raw';
 import IconResolveFilled from './icons/resolve-filled.svg?raw';
 import IconReverse from './icons/reverse.svg?raw';
+import IconRotateRight from './icons/rotate-right.svg?raw';
 import IconSearch from './icons/search.svg?raw';
-import IconSearchLarge from './icons/search-large.svg?raw';
+import IconSelectMatching from './icons/select-matching.svg?raw';
+import IconSelectMatchingAlt from './icons/select-matching-alt.svg?raw';
 import IconSettings from './icons/settings.svg?raw';
 import IconShare from './icons/share.svg?raw';
+import IconSidebarToggle from './icons/sidebar-toggle.svg?raw';
 import IconSmiley from './icons/smiley.svg?raw';
+import IconSolid from './icons/solid.svg?raw';
 import IconSortAlphaAsc from './icons/sort-alpha-asc.svg?raw';
 import IconSortAlphaDsc from './icons/sort-alpha-dsc.svg?raw';
 import IconSortTopBottom from './icons/sort-top-bottom.svg?raw';
-import IconSpacing from './icons/spacing.svg?raw';
 import IconSpinner from './icons/spinner.svg?raw';
 import IconStarOff from './icons/star-off.svg?raw';
 import IconStarOn from './icons/star-on.svg?raw';
@@ -97,19 +116,27 @@ import IconTidyUpListVertical from './icons/tidy-up-list-vertical.svg?raw';
 import IconTimer from './icons/timer.svg?raw';
 import IconTrash from './icons/trash.svg?raw';
 import IconUpDown from './icons/arrow-up-down.svg?raw';
-import IconVerticalPadding from './icons/vertical-padding.svg?raw';
+import IconVideo from './icons/video.svg?raw';
 import IconVisible from './icons/visible.svg?raw';
 import IconWarning from './icons/warning.svg?raw';
 import IconWarningLarge from './icons/warning-large.svg?raw';
+import IconLinkDetach from './icons/link-detach.svg?raw';
+
+// Node Icons
+import IconNodeArrow from './icons/node-types/Arrow.svg?raw';
 import IconNodeBooleanOperation from './icons/node-types/BooleanOperation.svg?raw';
-import IconNodeComponent from './icons/node-types/BooleanOperation.svg?raw';
+import IconNodeComponent from './icons/node-types/Component.svg?raw';
+import IconNodeComponentSet from './icons/node-types/ComponentSet.svg?raw';
 import IconNodeConnector from './icons/node-types/Connector.svg?raw';
 import IconNodeEllipse from './icons/node-types/Ellipse.svg?raw';
 import IconNodeFrame from './icons/node-types/Frame.svg?raw';
 import IconNodeGroup from './icons/node-types/Group.svg?raw';
 import IconNodeImage from './icons/node-types/Image.svg?raw';
 import IconNodeInstance from './icons/node-types/Instance.svg?raw';
+import IconNodeLine from './icons/node-types/Line.svg?raw';
 import IconNodePage from './icons/node-types/Page.svg?raw';
+import IconNodePolygon from './icons/node-types/Polygon.svg?raw';
+import IconNodeRectangle from './icons/node-types/Rectangle.svg?raw';
 import IconNodeSection from './icons/node-types/Section.svg?raw';
 import IconNodeSlice from './icons/node-types/Slice.svg?raw';
 import IconNodeStar from './icons/node-types/Star.svg?raw';
@@ -139,7 +166,10 @@ export {
 	IconAlignVerticalCenters,
 	IconAngle,
 	IconArrowLeftRight,
+	IconAutoLayoutAdd,
 	IconAutoLayoutHorizontal,
+	IconAutoLayoutIgnore,
+	IconAutoLayoutRemove,
 	IconAutoLayoutVertical,
 	IconBack,
 	IconBlend,
@@ -150,8 +180,11 @@ export {
 	IconCaretRight,
 	IconCaretUp,
 	IconCheck,
+	IconChevronDown,
+	IconChevronRight,
 	IconClose,
-	IconComponent,
+	IconCode,
+	IconComponentMultiple,
 	IconCornerRadius,
 	IconCorners,
 	IconDistributeHorizontalSpacing,
@@ -160,14 +193,17 @@ export {
 	IconEffects,
 	IconEllipses,
 	IconEyedropper,
+	IconFlatten,
+	IconFlipHorizontal,
+	IconFlipVertical,
+	IconFolder,
 	IconForward,
-	IconFrame,
-	IconGroup,
+	IconGapHorizontal,
+	IconGapVertical,
+	IconGradient,
 	IconHidden,
-	IconHorizontalPadding,
 	IconHyperlink,
 	IconImage,
-	IconInstance,
 	IconKey,
 	IconLayoutAlignBottom,
 	IconLayoutGridColumns,
@@ -181,7 +217,31 @@ export {
 	IconListTile,
 	IconLockOff,
 	IconLockOn,
+	IconMaskAdd,
+	IconMaskExclude,
+	IconMaskIntersect,
+	IconMaskSubtract,
+	IconMaskUnion,
 	IconMinus,
+	IconNodeArrow,
+	IconNodeBooleanOperation,
+	IconNodeComponent,
+	IconNodeComponentSet,
+	IconNodeConnector,
+	IconNodeEllipse,
+	IconNodeFrame,
+	IconNodeGroup,
+	IconNodeImage,
+	IconNodeInstance,
+	IconNodeLine,
+	IconNodePage,
+	IconNodePolygon,
+	IconNodeRectangle,
+	IconNodeSection,
+	IconNodeSlice,
+	IconNodeStar,
+	IconNodeText,
+	IconPathEdit,
 	IconPlay,
 	IconPlus,
 	IconRandom,
@@ -190,15 +250,18 @@ export {
 	IconResolve,
 	IconResolveFilled,
 	IconReverse,
+	IconRotateRight,
 	IconSearch,
-	IconSearchLarge,
+	IconSelectMatching,
+	IconSelectMatchingAlt,
 	IconSettings,
 	IconShare,
+	IconSidebarToggle,
 	IconSmiley,
+	IconSolid,
 	IconSortAlphaAsc,
 	IconSortAlphaDsc,
 	IconSortTopBottom,
-	IconSpacing,
 	IconSpinner,
 	IconStarOff,
 	IconStarOn,
@@ -212,21 +275,9 @@ export {
 	IconTimer,
 	IconTrash,
 	IconUpDown,
-	IconVerticalPadding,
+	IconVideo,
 	IconVisible,
 	IconWarning,
 	IconWarningLarge,
-	IconNodeBooleanOperation,
-	IconNodeComponent,
-	IconNodeConnector,
-	IconNodeEllipse,
-	IconNodeFrame,
-	IconNodeGroup,
-	IconNodeImage,
-	IconNodeInstance,
-	IconNodePage,
-	IconNodeSection,
-	IconNodeSlice,
-	IconNodeStar,
-	IconNodeText
+	IconLinkDetach
 };
